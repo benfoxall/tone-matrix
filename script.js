62c958c2186d49495d221b4d57bf2eb12879c98a
Ben Foxall <ben@whiteoctober.co.uk>
2015-01-19 22:30:40 +0000

    hacky sounds

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var frequencies = [100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200],
-    timings     = [0, 100, 200, 300, 400, 500, 600, 700, 800];
+var frequencies = [800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300],
+
+    duration    = 2000,
+    steps       = 10,
+    step        = duration/steps,
+    timings     = d3.range(step, duration, step);
 
 var matrix = timings.map(function(t, t_i){
   return frequencies.map(function(f, f_i){
@@ -49,10 +53,10 @@ items.enter()
   .append('rect')
   .attr('width', x(.9))
   .attr('height', y(.9))
-  .attr('x', function(d){return x(d.t_i)})
-  .attr('y', function(d){return y(d.f_i)})
   .attr('rx', x(.1))
   .attr('ry', y(.1))
+  .attr('x', function(d){return x(d.t_i)})
+  .attr('y', function(d){return y(d.f_i)})
   .style('fill', fill)
   .style('cursor', 'pointer')
   .on('click', function(data){
@@ -64,3 +68,71 @@ items.enter()
       .style('fill', fill)
   })
 
+
+
+// audio stuff
+// it's more important to be in sync, rather than continous
+
+// HACKY / TEMPORARY
+
+
+// queue a bar of music TEMP
+function queue(){
+  matrix.forEach(function(notes, i){
+    setTimeout(function(){
+      notes.filter(function(d){
+        return d.active
+      }).forEach(function(d){
+        play(d.f)
+      });
+
+      items
+        .style('fill', fill)
+
+        .filter(function(d){
+          if(notes.indexOf(d) > -1){
+            return true;
+          }
+        })
+        // .transition()
+        .style('fill', function(d){
+          return d.active ? '#f08' : '#ddd';
+        })
+
+
+
+    },step*i);
+  });
+
+  setTimeout(queue, duration-step);
+}
+
+queue();
+
+
+
+
+context = new webkitAudioContext();
+
+function play(f){
+
+  var now = context.currentTime;
+
+  var oscillator = context.createOscillator();
+
+  oscillator.frequency.value = f;
+  // Create sound source  
+
+  var gainNode = context.createGain();
+  gainNode.gain.value = 0;
+  gainNode.gain.exponentialRampToValueAtTime(1.0, now+0.2);
+  gainNode.gain.linearRampToValueAtTime(0, now+0.5);
+  oscillator.connect(gainNode);
+  gainNode.connect(context.destination);
+
+
+  // gainNode.start(0)
+  oscillator.start(now);
+  oscillator.stop(now+1);
+
+}
