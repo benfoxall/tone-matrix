c73c7256f39dda7b0c8decf04b3513d14995547d
Ben Foxall <ben@whiteoctober.co.uk>
2015-01-26 14:48:22 +0000

    add less->css to the build

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var uglify = require('gulp-uglify');
 var serve = require('gulp-serve');
 var connectlr = require('connect-livereload')();
 var livereload = require('gulp-livereload');
+var notify = require("gulp-notify");
+var less = require('gulp-less');
 
 
 gulp.task('js', function() {
@@ -12,10 +14,23 @@ gulp.task('js', function() {
   .pipe(sourcemaps.init())
     .pipe(concat('all.js'))
     .pipe(uglify())
+    .on('error', notify.onError({title:"js build error"}))
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('build'))
   .pipe(livereload());
 });
+
+
+gulp.task('css', function() {
+  gulp.src('./src/style.less')
+  .pipe(sourcemaps.init())
+  .pipe(less())
+  .on('error', notify.onError({title:"css build error"}))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('build'))
+  .pipe(livereload());
+});
+
 
 // don't actually do anything, but notify LR
 gulp.task('html', function() {
@@ -24,9 +39,10 @@ gulp.task('html', function() {
 })
 
 
-gulp.task('default', function(){
+gulp.task('default', ['js', 'css'], function(){
   livereload.listen();
   gulp.watch('src/*.js', ['js']);
+  gulp.watch('src/*.less', ['css']);
   gulp.watch('*.html',   ['html']);
 
   serve({
