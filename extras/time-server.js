baa529492d6f36ecae30a1f73b6a3efaa024b2ee
Ben Foxall <ben@whiteoctober.co.uk>
2015-01-26 16:51:34 +0000

    open for cors

--- extras/time-server.js
+++ extras/time-server.js
@@ -3,7 +3,12 @@ var http = require('http');
 var port = process.env.PORT || 4000;
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Timing-Allow-Origin': '*'
+  });
   res.end(''+Date.now());
 }).listen(process.env.PORT || 4000);
 
